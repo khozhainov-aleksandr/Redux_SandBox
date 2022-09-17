7de3ed02c343320cec2f60777507ab4933b7e6b1
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2022-09-17 22:36:00 +0300

    Done

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import { legacy_createStore as createStore} from 'redux'
 const redux = (state = 0, action) => {
   switch (action.type) {
     case 'INC':
-      return state += 1;
+      return state + 1;
+    case 'DEC':
+      return state - 1;
   
     default:
       return state;
@@ -12,13 +14,26 @@ const redux = (state = 0, action) => {
 
 const store = createStore(redux);
 
-// Следит за изменениями store и выводит console каждый раз как store изменится 
-store.subscribe(() => {
-  console.log('✅', store.getState());
-});
+document
+  .getElementById('inc')
+  .addEventListener('click', () => {
+    store.dispatch({
+      type: 'INC',
+    });
+  });
 
-store.dispatch({type: 'INC'});
-store.dispatch({type: 'INC'});
-store.dispatch({type: 'INC'});
-store.dispatch({type: 'INC'});
-store.dispatch({type: 'INC'});
+document
+  .getElementById('dec')
+  .addEventListener('click', () => {
+    store.dispatch({
+      type: 'DEC',
+    });
+  });
+
+const update = () => {
+  document
+    .getElementById('counter')
+    .innerHTML = store.getState();
+};
+
+store.subscribe(update);
