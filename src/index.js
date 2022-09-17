1313be4ca8c9e49bd5398e3297d124afc0b0c500
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2022-09-17 22:19:54 +0300

    Done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-console.log('✅', 'Test string');
+import { legacy_createStore as createStore} from 'redux'
+
+const redux = (state = 0, action) => {
+  switch (action.type) {
+    case 'INC':
+      return state += 1;
+  
+    default:
+      return state;
+  }
+}
+
+const store = createStore(redux);
+
+// Следит за изменениями store и выводит console каждый раз как store изменится 
+store.subscribe(() => {
+  console.log('✅', store.getState());
+});
+
+store.dispatch({type: 'INC'});
+store.dispatch({type: 'INC'});
+store.dispatch({type: 'INC'});
+store.dispatch({type: 'INC'});
+store.dispatch({type: 'INC'});
