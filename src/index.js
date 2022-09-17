494b7c673122b1e2776fb5162752abce3d8e575f
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2022-09-17 22:44:43 +0300

    Done

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ const redux = (state = 0, action) => {
       return state + 1;
     case 'DEC':
       return state - 1;
+    case 'RND':
+      return state + action.payload;
   
     default:
       return state;
@@ -27,6 +29,16 @@ document
   .addEventListener('click', () => {
     store.dispatch({
       type: 'DEC',
+    });
+  });
+
+document
+  .getElementById('rnd')
+  .addEventListener('click', () => {
+    const payload = Math.floor(Math.random() * 100);
+    store.dispatch({
+      type: 'RND',
+      payload: payload,
     });
   });
 
