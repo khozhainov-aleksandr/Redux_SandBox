3c5c70900c04009e79b2ee9d0d2a88617bca2924
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2022-09-17 18:58:20 +0300

    Done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
-console.log('✅', 'Test string');
+const reducer = (state = 0, action) => {
+  switch (action.type) {
+    case 'INC':
+      return state += 1;
+  
+    default:
+      return state;
+  }
+}
+
+let state = reducer(undefined, {});
+console.log('✅', state); // 0
+
+state = reducer(state, {type: 'INC'});
+console.log('✅', state); // 1
+
+state = reducer(state, {type: 'INC'});
+console.log('✅', state); // 2
+
+state = reducer(state, {type: 'INC'});
+console.log('✅', state); // 3
+
+state = reducer(state, {type: 'INC'});
+console.log('✅', state); // 4
+
+state = reducer(state, {type: 'INC'});
+console.log('✅', state); // 5
+
+state = reducer(state, {type: 'INC'});
+console.log('✅', state); // 6
